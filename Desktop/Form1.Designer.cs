b21940305df553ed3beef10d7b2c14380fc5e119
TiBO314 <exequiel.miranda314@gmail.com>
2021-12-28 16:41:31 -0600

    commit prueba 2

--- Desktop/Form1.Designer.cs
+++ Desktop/Form1.Designer.cs
@@ -46,18 +46,18 @@ namespace Desktop
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(462, 332);
+            this.label1.Location = new System.Drawing.Point(462, 293);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 22);
+            this.label1.Size = new System.Drawing.Size(0, 21);
             this.label1.TabIndex = 2;
             // 
             // btnIngresar
             // 
             this.btnIngresar.BackColor = System.Drawing.Color.DarkTurquoise;
             this.btnIngresar.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnIngresar.Location = new System.Drawing.Point(543, 577);
+            this.btnIngresar.Location = new System.Drawing.Point(543, 509);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(145, 39);
+            this.btnIngresar.Size = new System.Drawing.Size(145, 34);
             this.btnIngresar.TabIndex = 10;
             this.btnIngresar.Text = "INGRESAR";
             this.btnIngresar.UseVisualStyleBackColor = false;
@@ -66,17 +66,17 @@ namespace Desktop
             // txtContraseña
             // 
             this.txtContraseña.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtContraseña.Location = new System.Drawing.Point(478, 450);
+            this.txtContraseña.Location = new System.Drawing.Point(478, 397);
             this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.Size = new System.Drawing.Size(328, 30);
+            this.txtContraseña.Size = new System.Drawing.Size(328, 29);
             this.txtContraseña.TabIndex = 9;
             // 
             // txtUsuario
             // 
             this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtUsuario.Location = new System.Drawing.Point(478, 365);
+            this.txtUsuario.Location = new System.Drawing.Point(478, 322);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(328, 30);
+            this.txtUsuario.Size = new System.Drawing.Size(328, 29);
             this.txtUsuario.TabIndex = 8;
             // 
             // label2
@@ -84,9 +84,9 @@ namespace Desktop
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(346, 454);
+            this.label2.Location = new System.Drawing.Point(346, 401);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 22);
+            this.label2.Size = new System.Drawing.Size(114, 21);
             this.label2.TabIndex = 7;
             this.label2.Text = "CONTRASEÑA:";
             // 
@@ -95,9 +95,9 @@ namespace Desktop
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(386, 369);
+            this.label3.Location = new System.Drawing.Point(386, 326);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 22);
+            this.label3.Size = new System.Drawing.Size(80, 21);
             this.label3.TabIndex = 6;
             this.label3.Text = "USUARIO:";
             // 
@@ -105,9 +105,9 @@ namespace Desktop
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(518, 110);
+            this.pictureBox1.Location = new System.Drawing.Point(518, 97);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(209, 193);
+            this.pictureBox1.Size = new System.Drawing.Size(209, 170);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
@@ -115,20 +115,20 @@ namespace Desktop
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(628, 45);
+            this.label4.Location = new System.Drawing.Point(628, 40);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.Size = new System.Drawing.Size(146, 15);
             this.label4.TabIndex = 12;
-            this.label4.Text = "Prueba";
+            this.label4.Text = "Prueba - Exequiel miranda";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1220, 849);
+            this.ClientSize = new System.Drawing.Size(1220, 661);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnIngresar);
